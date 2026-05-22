9cceb81ba38eb59215b4d33c80a69feb7867bd61
Sudipto Chandra <dipu.sudipta@gmail.com>
2026-05-22 17:04:04 +0400

    Refactor browser template to use "nodriver" instead of "selenium"

--- lncrawl/webdriver/__init__.py
+++ lncrawl/webdriver/__init__.py
@@ -1,12 +1,8 @@
-# https://cloudbytes.dev/snippets/run-selenium-and-chrome-on-wsl2
-# https://github.com/ultrafunkamsterdam/undetected-chromedriver
-
 import logging
 import os
-from typing import Optional
+from typing import List, Optional
 
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.remote.webdriver import WebDriver
+import nodriver
 
 from ..context import ctx
 
@@ -14,30 +10,21 @@ logger = logging.getLogger(__name__)
 
 
 def create_new(
-    options: Optional[ChromeOptions] = None,
+    extra_args: Optional[List[str]] = None,
     timeout: Optional[float] = None,
     user_data_dir: Optional[str] = None,
     headless: bool = False,
     **kwargs,
-) -> WebDriver:
-    """Create a new webdriver instance"""
+) -> nodriver.Browser:
+    """Create a new nodriver browser instance."""
     if not user_data_dir:
         user_data_dir = str(ctx.config.app.app_dir / "webdriver")
         os.makedirs(user_data_dir, exist_ok=True)
-    if ctx.config.crawler.selenium_grid:
-        from .remote import create_remote
+    from .local import create_local
 
-        return create_remote(
-            address=ctx.config.crawler.selenium_grid,
-            options=options,
-            timeout=timeout,
-        )
-    else:
-        from .local import create_local
-
-        return create_local(
-            options=options,
-            timeout=timeout,
-            user_data_dir=user_data_dir,
-            headless=headless,
-        )
+    return create_local(
+        extra_args=extra_args,
+        timeout=timeout,
+        user_data_dir=user_data_dir,
+        headless=headless,
+    )
